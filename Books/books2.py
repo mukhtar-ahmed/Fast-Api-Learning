eb6a6a9fb9bbc65e4030d8987dd56ef3fcfcc47f
mukhtar-ahmed <kh.mukhtar60@gmail.com>
2025-02-05 20:47:36 +0400

    book2 complete

--- Books/books2.py
+++ Books/books2.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
+from starlette import status
+
 app = FastAPI()
 
 class Book:
@@ -8,13 +10,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
     
-    def __init__(self,id,title,author,description,rating):
+    def __init__(self,id,title,author,description,rating,published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 class BookIn(BaseModel):
     id:int
@@ -22,6 +26,7 @@ class BookIn(BaseModel):
     author: str = Field(min_length=3)
     description: str = Field(min_length=10)
     rating: int = Field(gt=0, lt=6)
+    published_date: int 
     
     model_config = {
         "json_schema_extra":{
@@ -30,19 +35,20 @@ class BookIn(BaseModel):
                 "title": "Book1",
                 "author": "Author1",
                 "description": "This is a book",
-                "rating": 5
+                "rating": 5,
+                "published_date": 2019
             }
         }
     }
 
 BOOKS = [
-    Book(1,'Dev ops' , 'Mukhtar Ahmed','Its nice book',5),
-    Book(2,'Python' , 'Mukhtar Ahmed','Its nice book',4),
-    Book(3,'Java' , 'Mukhtar Ahmed','Its nice book',3),
-    Book(3,'c++' , 'Awais','Average',5),
+    Book(1,'Dev ops' , 'Mukhtar Ahmed','Its nice book',5,2019),
+    Book(2,'Python' , 'Mukhtar Ahmed','Its nice book',4,2020),
+    Book(3,'Java' , 'Mukhtar Ahmed','Its nice book',3,2019),
+    Book(3,'c++' , 'Awais','Average',5,2015),
 ]
 
-@app.get('/books')
+@app.get('/books', status_code=status.HTTP_200_OK)
 def read_books():
     return {
         'message': 'Sucess',
@@ -50,7 +56,7 @@ def read_books():
         'data': BOOKS
     }
     
-@app.post("/books/create-book")
+@app.post("/books/create-book" , status_code=status.HTTP_201_CREATED)
 def create_book(book : BookIn):
     BOOKS.append(find_book_id(Book(**book.model_dump())))
     return {
@@ -59,7 +65,19 @@ def create_book(book : BookIn):
         'data': BOOKS
     }
     
-@app.get('/books/{book_id}')
+# @app.get('/books/{book_published_date}', status_code=status.HTTP_200_OK)
+# def get_books_by_published_date(book_published_date:int=Path(gt=1999, lt=2026)):
+#     return_book = []
+#     for book in BOOKS:
+#         if book.published_date == book_published_date:
+#             return_book.append(book)
+#     return {
+#         'message': 'Sucess',
+#         'total': len(return_book),
+#         'data': return_book
+#     }
+    
+@app.get('/books/{book_id}', status_code=status.HTTP_200_OK)
 def read_book(book_id:int):
     for book in BOOKS:
         if book.id == book_id:
@@ -67,7 +85,7 @@ def read_book(book_id:int):
                 'message': 'Sucess',
                 'data': book
                 }
-    return {'message': 'Book not found'}
+    return {'message': 'Book not found', 'status_code':HTTPException(status_code=400, detail='Not Found')}
 
 
 
@@ -85,11 +103,13 @@ async def bookByRating(book_by_rating: int):
 
 
 
-@app.put("/books/{book_id}")
-async def update_book(book_id: int, book: BookIn):
-    for i, book in enumerate(BOOKS):
-        if book.id == book_id:
-            BOOKS[i] = book.dict()
+@app.put("/books/update_book")
+async def update_book(book: BookIn):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i] == book.id:
+            BOOKS[i] = book
+            book_changed = True
             return {
                 'message': 'Sucess',
                 'data': BOOKS[i]
