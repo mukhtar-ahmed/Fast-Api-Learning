72c1be14fc3505dcee25a450807b37c82f6fab1a
mukhtar-ahmed <kh.mukhtar60@gmail.com>
2025-02-11 22:05:40 +0400

    11/02/2025

--- Todo_App/main.py
+++ Todo_App/main.py
@@ -6,89 +6,11 @@ from fastapi import FastAPI , Depends , HTTPException ,Path
 from models import Base, Todos
 from database import engine , SessionLocal
 from sqlalchemy.exc import SQLAlchemyError
-from routers import auth
+from routers import auth, todos
 
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(auth.router)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
-db_dependency = Annotated[Session, Depends(get_db)]
-
-class TodoIn(BaseModel):
-    title: str = Field(min_length=3)
-    description: str = Field(min_length=3, max_length=100)
-    priority: int = Field(gt=0,lt=6)
-    complete:bool
-        
-@app.get('/', status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
-
-@app.get('/todos/{todo_id}')
-async def read_todo_by_id(db: db_dependency, todo_id:int=Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
-    if todo_model is not None:
-        return todo_model
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
-    
-@app.post("/todo/", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo:TodoIn):
-    todo_obj = Todos(**todo.model_dump())
-    db.add(todo_obj)
-    try:
-        db.commit()
-        db.refresh(todo_obj)
-    except SQLAlchemyError as e:
-        db.rollback()
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error creating todo")
-    return {
-        'message': 'todo created',
-        'data': todo_obj
-    }
-    
-@app.put('/todo/{id}', status_code=status.HTTP_200_OK)
-async def update_todo(db: db_dependency,todo:TodoIn, id:int = Path(gt=0)):
-    todo_obj = db.query(Todos).filter(Todos.id == id).first()
-    if todo_obj is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Todo not found')
-    else:
-        todo_obj.title = todo.title
-        todo_obj.description = todo.description
-        todo_obj.priority = todo.priority
-        todo_obj.complete = todo.complete
-        
-        try:
-            db.commit()
-            db.refresh(todo_obj)
-        except SQLAlchemyError as e:
-            db.rollback()
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error updating todo")
-        return {
-            'message': 'todo updated',
-            'data': todo_obj
-        }
-        
-@app.delete('/todo/{todo_id}',status_code=status.HTTP_204_NO_CONTENT)
-async def delete_todo(db: db_dependency, todo_id:int=Path(gt=0)):
-    todo_obj = db.query(Todos).filter(Todos.id == todo_id).first()
-    if todo_obj is None:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail='Todo Not Found')
-    else:
-        db.delete(todo_obj)
-        try:
-            db.commit()
-        except SQLAlchemyError as e:
-            db.rollback()
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error Deleting todo")
-        
-            
+app.include_router(todos.router)
--- Todo_App/routers/auth.py
+++ Todo_App/routers/auth.py
@@ -1,6 +1,64 @@
-from fastapi import APIRouter
+from typing import Annotated
+from starlette import status
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel, Field
+from models import Users
+from database import SessionLocal
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+
+
+
 router = APIRouter()
 
-@router.get('/hello')
-async def read_user():
-    return {"message": "Hello, World!"}
+class UserIn(BaseModel):
+    email:str = Field(min_length=3)
+    username:str =Field(min_length=3)
+    first_name:str=Field(min_length=3)
+    last_name:str=Field(min_length=3)
+    password:str=Field(min_length=8)
+    role:str
+    
+    model_config= {
+        "json_schema_extra":{
+            "example":{
+                "email":"user@example.com",
+                "username":"user123",
+                "first_name":"John",
+                "last_name":"Doe",
+                "password":"password123",
+                "role":"admin"  
+            }
+        }
+    }
+    
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+db_dependency = Annotated[Session, Depends(get_db)]
+bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
+
+@router.post('/auth',status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency ,user:UserIn):
+    user = Users(
+        email=user.email,
+        username=user.username,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        hashed_password=bcrypt_context.hash( user.password),
+        role=user.role
+    )
+    db.add(user)
+    try:
+        db.commit()
+        db.refresh(user)
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Error While adding user')
+    return user
+    
+    
